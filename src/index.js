4cc44c7801277368961f9f8a8927c98346ad138d
Vlad <guf_of_vlad@mail.ru>
2018-02-23 02:18:54 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let arrayFromStr = str.split('');
+    let pairsOfBrackets = {};
+    bracketsConfig.forEach((value) => { pairsOfBrackets[value[0]] = value[1]; });
+    let stack = [];
+
+    for (let i = 0; i < arrayFromStr.length; i++) {
+
+        let k = pairsOfBrackets[arrayFromStr[i]];
+        let x = pairsOfBrackets[stack[stack.length-1]];
+
+        if((k === x) && (pairsOfBrackets[x] === x) && stack.length !== 0) { // fist and second checks will only true If open bracket == close Bracket
+            stack.pop();
+            continue;
+        }
+
+        if (arrayFromStr[i] in pairsOfBrackets) {
+            stack.push(arrayFromStr[i]);
+        }
+        else {
+            if (stack.length === 0 ||  pairsOfBrackets[stack.pop()] !== arrayFromStr[i])
+                return false;
+        }
+    }
+
+    return !stack.length;
+};
